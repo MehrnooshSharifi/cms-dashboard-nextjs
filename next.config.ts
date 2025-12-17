f2c611afa90ddb0705d35a2acbdbaba5d3423e43
Mehrnoosh <me88.sharifi@gmail.com>
2025-12-17 09:57:50 +0330

    tsConfig has some changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: false,
-  },
   env: {
     NEXT_DISABLE_TURBOPACK: "1",
   },
